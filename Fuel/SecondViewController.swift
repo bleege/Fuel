689dc022a77c97debd3d96ed52fc74ccea08abd3
Brad Leege <bleege@gmail.com>
2017-11-21 21:27:14 -0600

    Initial Commit

---
+++ Fuel/SecondViewController.swift
@@ -0,0 +1,25 @@
+//
+//  SecondViewController.swift
+//  Fuel
+//
+//  Created by Brad Leege on 11/21/17.
+//  Copyright © 2017 Brad Leege. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
