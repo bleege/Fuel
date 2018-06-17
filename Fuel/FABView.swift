a98c3f1e09ffd6f655d7135eee8cb6c791313558
Brad Leege <bleege@gmail.com>
2018-06-16 23:24:24 -0500

    Drawing plus sign on FAB

--- Fuel/FABView.swift
+++ Fuel/FABView.swift
@@ -16,10 +16,27 @@ class FABView: UIView {
     }
     
     override func draw(_ rect: CGRect) {
-        // Drawing code
+        super.draw(rect)
+        
+        // Circle
         UIColor.green.setFill()
         let circle = UIBezierPath(ovalIn: rect)
         circle.fill()
+        
+        if let context = UIGraphicsGetCurrentContext() {
+            context.setStrokeColor(UIColor.white.cgColor)
+            context.setLineWidth(2)
+            
+            // Horizontal
+            context.move(to: CGPoint(x: bounds.width / 4, y: bounds.height / 2))
+            context.addLine(to: CGPoint(x: bounds.width * 0.75, y: bounds.height / 2))
+            
+            // Vertical
+            context.move(to: CGPoint(x: bounds.width / 2, y: bounds.height / 4))
+            context.addLine(to: CGPoint(x: bounds.width / 2, y: bounds.height * 0.75))
+            
+            context.strokePath()
+        }
     }
 
 }
