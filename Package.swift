d470e1d9dfa27c8fc039428970ae5b7b03475f2d
Brad Leege <bleege@gmail.com>
2019-10-08 20:07:50 -0500

    Getting Tests To Work with Swift Package Manager

--- Package.swift
+++ Package.swift
@@ -4,25 +4,28 @@
 import PackageDescription
 
 let package = Package(
-    name: "MyLibrary",
+    name: "Fuel",
     products: [
         // Products define the executables and libraries produced by a package, and make them visible to other packages.
         .library(
-            name: "MyLibrary",
-            targets: ["MyLibrary"]),
+            name: "Fuel",
+            targets: ["Fuel"]),
     ],
     dependencies: [
         // Dependencies declare other packages that this package depends on.
         // .package(url: /* package url */, from: "1.0.0"),
+        .package(url: "https://github.com/ReactiveX/RxSwift.git", from: "5.0.0"),
+        .package(url: "https://github.com/Swinject/Swinject.git", from: "2.6.0"),
+        .package(url: "https://github.com/Swinject/SwinjectAutoregistration.git", from: "2.6.0")        
     ],
     targets: [
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
         // Targets can depend on other targets in this package, and on products in packages which this package depends on.
         .target(
-            name: "MyLibrary",
-            dependencies: []),
+            name: "Fuel",
+            dependencies: ["RxSwift", "Swinject", "SwinjectAutoregistration"])
         .testTarget(
-            name: "MyLibraryTests",
-            dependencies: ["MyLibrary"]),
+            name: "FuelTests",
+            dependencies: ["Fuel"]),
     ]
 )
