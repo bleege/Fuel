654f6bdaf5439707338df376a0df5631c4ceaffb
Brad Leege <bleege@gmail.com>
2019-11-03 18:17:16 -0600

    Conversion to code for StopDetailViewController

--- Fuel/StopDetailViewController.swift
+++ Fuel/StopDetailViewController.swift
@@ -11,20 +11,70 @@ import UIKit
 
 class StopDetailViewController: UIViewController {
     
-    @IBOutlet weak var stopDateLabel: UILabel!
-    @IBOutlet weak var ppgLabel: UILabel!
-    @IBOutlet weak var gallonsLabel: UILabel!
-    @IBOutlet weak var costLabel: UILabel!
-    @IBOutlet weak var tripOdometerLabel: UILabel!
-    @IBOutlet weak var tripMPGLabel: UILabel!
-    @IBOutlet weak var odometerLabel: UILabel!
-    @IBOutlet weak var octaneLabel: UILabel!
+    // MARK: - Data Presentation Views
+    private let stopDateLabel: UILabel = {
+        let label = UILabel()
+        label.font = UIFont.systemFont(ofSize: 17.0, weight: .bold)
+        label.translatesAutoresizingMaskIntoConstraints = false
+        return label
+    }()
     
+    private let ppgLabel: UILabel = {
+        let label = UILabel()
+        label.font = UIFont.systemFont(ofSize: 30.0)
+        label.translatesAutoresizingMaskIntoConstraints = false
+        return label
+    }()
+
+    private let gallonsLabel: UILabel = {
+        let label = UILabel()
+        label.font = UIFont.systemFont(ofSize: 30.0)
+        label.translatesAutoresizingMaskIntoConstraints = false
+        return label
+    }()
+
+    private let costLabel: UILabel = {
+        let label = UILabel()
+        label.font = UIFont.systemFont(ofSize: 30.0)
+        label.translatesAutoresizingMaskIntoConstraints = false
+        return label
+    }()
+
+    private let tripOdometerLabel: UILabel = {
+        let label = UILabel()
+        label.font = UIFont.systemFont(ofSize: 30.0)
+        label.translatesAutoresizingMaskIntoConstraints = false
+        return label
+    }()
+
+    private let tripMPGLabel: UILabel = {
+        let label = UILabel()
+        label.font = UIFont.systemFont(ofSize: 30.0)
+        label.translatesAutoresizingMaskIntoConstraints = false
+        return label
+    }()
+
+    private let odometerLabel: UILabel = {
+        let label = UILabel()
+        label.font = UIFont.systemFont(ofSize: 30.0)
+        label.translatesAutoresizingMaskIntoConstraints = false
+        return label
+    }()
+
+    private let octaneLabel: UILabel = {
+        let label = UILabel()
+        label.font = UIFont.systemFont(ofSize: 30.0)
+        label.translatesAutoresizingMaskIntoConstraints = false
+        return label
+    }()
+        
     var stopData: FuelStop?
     
     override func viewDidLoad() {
         super.viewDidLoad()
      
+        setupViewHierarchy()
+        
         stopDateLabel.text = stopData?.stopDate.longFormat()
         ppgLabel.text = stopData?.pricePerGallon.currencyFormat()
         gallonsLabel.text = stopData?.gallons.gallonFormat()
@@ -40,6 +90,85 @@ class StopDetailViewController: UIViewController {
         
         setupTapToDismissRecognizer()
     }
+    
+    // MARK: - View Hierarchy Setup
+    private func setupViewHierarchy() {
+        view.addSubview(stopDateLabel)
+        
+        let topStack = UIStackView()
+        topStack.axis = .horizontal
+        topStack.alignment = .fill
+        topStack.distribution = .fillProportionally
+        topStack.spacing = 0
+        topStack.addArrangedSubview(createPairingStack(dataLabel: costLabel, descriptionText: "Price"))
+        topStack.addArrangedSubview(createPairingStack(dataLabel: gallonsLabel, descriptionText: "Gallons"))
+        topStack.addArrangedSubview(createPairingStack(dataLabel: ppgLabel, descriptionText: "$ / Gallon"))
+
+        view.addSubview(topStack)
+        
+        let bottomLeftStack = UIStackView()
+        bottomLeftStack.axis = .vertical
+        bottomLeftStack.alignment = .fill
+        bottomLeftStack.distribution = .equalCentering
+        bottomLeftStack.spacing = 0
+        bottomLeftStack.addArrangedSubview(createPairingStack(dataLabel: tripOdometerLabel, descriptionText: "Trip Odometer"))
+        bottomLeftStack.addArrangedSubview(createPairingStack(dataLabel: odometerLabel, descriptionText: "Odometer"))
+
+        let bottomRightStack = UIStackView()
+        bottomRightStack.axis = .vertical
+        bottomRightStack.alignment = .fill
+        bottomRightStack.distribution = .equalCentering
+        bottomRightStack.spacing = 0
+        bottomRightStack.addArrangedSubview(createPairingStack(dataLabel: tripMPGLabel, descriptionText: "Trip MPG"))
+        bottomRightStack.addArrangedSubview(createPairingStack(dataLabel: octaneLabel, descriptionText: "Octane"))
+
+        let bottomStack = UIStackView()
+        bottomStack.axis = .horizontal
+        bottomStack.alignment = .fill
+        bottomStack.distribution = .fillEqually
+        bottomStack.spacing = 0
+        bottomStack.addArrangedSubview(bottomLeftStack)
+        bottomStack.addArrangedSubview(bottomRightStack)
+        
+        view.addSubview(bottomStack)
+
+        // TODO - Layout Constraints
+        NSLayoutConstraint.activate([
+            stopDateLabel.topAnchor.constraint(equalTo: view.topAnchor, constant: 16.0),
+            stopDateLabel.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 16.0),
+            stopDateLabel.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -16.0),
+            topStack.topAnchor.constraint(equalTo: stopDateLabel.bottomAnchor, constant: 20),
+            topStack.leadingAnchor.constraint(equalTo: stopDateLabel.leadingAnchor),
+            topStack.trailingAnchor.constraint(equalTo: stopDateLabel.trailingAnchor),
+            bottomStack.topAnchor.constraint(equalTo: topStack.bottomAnchor, constant: 28.0),
+            bottomStack.leadingAnchor.constraint(equalTo: topStack.leadingAnchor),
+            bottomStack.trailingAnchor.constraint(equalTo: topStack.trailingAnchor),
+//            bottomStack.bottomAnchor.constraint(equalTo: view.bottomAnchor)
+        ])        
+    }
+    
+    private func createPairingStack(dataLabel: UILabel, descriptionText: String) -> UIStackView {
+        
+        let stack = UIStackView()
+        stack.axis = .vertical
+        stack.alignment = .fill
+        stack.distribution = .fill
+        stack.spacing = 0.0
+        stack.addArrangedSubview(dataLabel)
+        stack.addArrangedSubview(createDescriptionLabel(text: descriptionText))
+        
+        return stack
+    }
+
+    private func createDescriptionLabel(text: String) -> UILabel {
+        let label = UILabel()
+        label.font = UIFont.systemFont(ofSize: 17.0)
+        label.text = text
+        label.translatesAutoresizingMaskIntoConstraints = false
+        return label
+    }
+
+    // MARK: - Dismiss Screen Gesture Handling
     
     private func setupTapToDismissRecognizer() {
         let tapRecognizer = UITapGestureRecognizer(target: self, action: #selector(handleTap(gestureRecognizer:)))
