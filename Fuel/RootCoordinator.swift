cdd8544d857ec451f55333e62925b7b0ef343bfe
Brad Leege <bleege@gmail.com>
2021-07-03 14:02:35 -0500

    Stubbing Coordinator and RootCoordinator

---
+++ Fuel/RootCoordinator.swift
@@ -0,0 +1,18 @@
+//
+//  RootCoordinator.swift
+//  Fuel
+//
+//  Created by Brad Leege on 7/3/21.
+//  Copyright © 2021 Brad Leege. All rights reserved.
+//
+
+import Foundation
+
+class RootCoordinator: Coordinator {
+
+
+    func start() {
+        
+    }
+    
+}
